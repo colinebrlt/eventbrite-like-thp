2c4b5738b8b87f2d5c98c1a8bc11101d2d07663d
Coline Brault <coline.brault@gmail.com>
2020-11-03 17:56:27 +0100

    with navbar and footer in different files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,16 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
 
   def show
+    check_user
     @user = User.find_by_id(params[:id])
     @admin_events = @user.admin_events
   end 
 
-  def create
-    
-  end 
+  def check_user
+    @user = User.find(params[:id])
+    unless current_user.id == @user.id
+       redirect_to root_path
+    end
+  end
+
 end
